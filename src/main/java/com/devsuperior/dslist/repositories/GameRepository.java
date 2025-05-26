d059ea01e137665ff634049ec02d56bcdd3ed096
Vitor <vitordevestudos@gmail.com>
2025-05-25 23:19:58 -0300

    Homolog testado com sucesso - OK

--- src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
+++ src/main/java/com/devsuperior/dslist/repositories/GameRepository.java
@@ -12,7 +12,7 @@ import java.util.List;
 // Dentro dos <> é usado como primeiro parametro nossa entidade e depois o tipo do ID
 public interface GameRepository extends JpaRepository<Game, Long> {
     @Query(nativeQuery = true, value = """
-		SELECT tb_game.id, tb_game.title, tb_game.game_year AS `year`, tb_game.img_url AS imgUrl,
+		SELECT tb_game.id, tb_game.title, tb_game.game_year AS gameYear, tb_game.img_url AS imgUrl,
 		tb_game.short_description AS shortDescription, tb_belonging.position
 		FROM tb_game
 		INNER JOIN tb_belonging ON tb_game.id = tb_belonging.game_id
